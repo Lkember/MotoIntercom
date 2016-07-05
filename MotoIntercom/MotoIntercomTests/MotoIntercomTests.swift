4c7208f5451a277ac070b98647edc203784f1508
Logan Kember <lkember@uwo.ca>
2016-07-05 19:08:48 -0400

    Initial commit

---
+++ MotoIntercom/MotoIntercomTests/MotoIntercomTests.swift
@@ -0,0 +1,36 @@
+//
+//  MotoIntercomTests.swift
+//  MotoIntercomTests
+//
+//  Created by Logan Kember on 2016-07-05.
+//  Copyright © 2016 Logan Kember. All rights reserved.
+//
+
+import XCTest
+@testable import MotoIntercom
+
+class MotoIntercomTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
