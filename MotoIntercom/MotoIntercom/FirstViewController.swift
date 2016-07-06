ca9a7e1c97058d2e03382cd52ac5c69c447589cc
Logan Kember <logan.916@hotmail.com>
2016-07-06 13:23:54 -0400

    Added objects that work in code

--- MotoIntercom/MotoIntercom/FirstViewController.swift
+++ MotoIntercom/MotoIntercom/FirstViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    // MARK: Properties
+    @IBOutlet weak var randLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    // MARK: Actions
+    @IBAction func setRandomLabel(sender: UIButton) {
+        randLabel.text = textField.text;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 }
 
