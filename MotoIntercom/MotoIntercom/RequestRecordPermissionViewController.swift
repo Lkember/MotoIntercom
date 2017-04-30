bdc1d177accb11d159bfa733327e4c6db015d43f
Logan Kember <lkember@ca.ibm.com>
2017-04-30 17:31:34 -0400

    Added a check when the app first starts to see if the user has given the app permission to use the microphone. If it hasn't given permission it will ask the user to allow permission.

---
+++ MotoIntercom/MotoIntercom/RequestRecordPermissionViewController.swift
@@ -0,0 +1,106 @@
+//
+//  RequestRecordPermissionViewController.swift
+//  MotoIntercom
+//
+//  Created by Logan Kember on 2017-04-28.
+//  Copyright © 2017 Logan Kember. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RequestRecordPermissionViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet var backgroundView: UIView!
+    @IBOutlet weak var popUpView: UIView!
+    let audioSession = AVAudioSession.sharedInstance()
+    
+    // MARK: - View
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if !UIAccessibilityIsReduceTransparencyEnabled() {
+            self.backgroundView.backgroundColor = UIColor.clear
+            
+            let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+            
+            blurEffectView.frame = self.backgroundView.bounds
+            blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            
+            self.backgroundView.insertSubview(blurEffectView, at: 0)
+        }
+        else {
+            self.backgroundView.backgroundColor = UIColor.black
+            self.backgroundView.alpha = 0.80
+        }
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        self.popUpView.layer.cornerRadius = 10
+        self.popUpView.layer.borderColor = UIColor.white.cgColor
+        self.popUpView.layer.borderWidth = 2
+        
+        self.animate()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Button Select
+    @IBAction func didTouchOk(_ sender: UIButton) {
+        
+        audioSession.requestRecordPermission({ (granted: Bool) -> Void in
+            if granted {
+                print("\(#file) > \(#function) > User granted access.")
+            }
+            else {
+                print("\(#file) > \(#function) > User denied access")
+            }
+        })
+        
+        dismissAnimate()
+    }
+    
+    
+    // MARK: - Animation
+    func animate() {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    
+    func dismissAnimate()
+    {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MotoIntercom/MotoIntercom/StartupViewController.swift
+++ MotoIntercom/MotoIntercom/StartupViewController.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class StartupViewController: UIViewController {
     
     // MARK: Properties
-    
+    var audioSession: AVAudioSession = AVAudioSession.sharedInstance()
     
     // MARK: Actions
     @IBAction func startSearching(_ sender: UIButton) {
-//        print("Changing to ChatView")
+        // Nothing to do
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -23,6 +24,25 @@ class StartupViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        print("\(#file) > \(#function) > Entry")
+        super.viewDidLoad()
         
+        let recordPermission = audioSession.recordPermission()
+        if recordPermission == AVAudioSessionRecordPermission.undetermined {
+            print("\(#file) > \(#function) > Record permission is undetermined")
+            
+            let popOverView = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "RequestMicrophoneAccess") as! RequestRecordPermissionViewController
+            self.addChildViewController(popOverView)
+            
+            popOverView.view.frame = self.view.frame
+            self.view.addSubview(popOverView.view)
+            popOverView.didMove(toParentViewController: self)
+        }
+        else if recordPermission == AVAudioSessionRecordPermission.denied {
+            print("\(#file) > \(#function) > Record permission is denied. Phone calls will be disabled.")
+        }
+        else if recordPermission == AVAudioSessionRecordPermission.granted {
+            print("\(#file) > \(#function) > Record permission is granted.")
+        }
     }
 }
