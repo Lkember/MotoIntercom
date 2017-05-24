8544f599396699cbe6f50bf77595ad61f17b8d8b
Logan Kember <lkember@ca.ibm.com>
2017-05-24 14:08:45 -0400

    Added print statements and removed unnecessary code.

--- MotoIntercom/MotoIntercom/AppDelegate.swift
+++ MotoIntercom/MotoIntercom/AppDelegate.swift
@@ -34,7 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         if (!didLoad) {
             connectionManager = ConnectionManager()
-            
             saveIdentification(peer: connectionManager.peer, uniqueID: connectionManager.uniqueID)
         }
         else {
@@ -46,19 +45,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
+        print("\(#file) > \(#function) > resigned active")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        connectionManager.advertiser.startAdvertisingPeer()
+        connectionManager.browser.stopBrowsingForPeers()
+        
+        print("\(#file) > \(#function) > Entered background")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        
+        connectionManager.advertiser.startAdvertisingPeer()
+        connectionManager.browser.startBrowsingForPeers()
+        
+        print("\(#file) > \(#function) > Entered foreground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("\(#file) > \(#function) > Became active")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
@@ -75,33 +87,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     
     // MARK: Saving
-    
-//    func saveUniqueID(uniqueID : String) {
-//        print("\(#file) > \(#function) > Saving uniqueID: \(uniqueID)")
-//        
-//        let data = NSKeyedArchiver.archivedData(withRootObject: uniqueID)
-//        
-//        let defaults = UserDefaults.standard
-//        defaults.set(data, forKey: uniqueIDString)
-//    }
-//    
-//    func loadUniqueID() -> Bool {
-//        print("\(#file) > \(#function) > Entry")
-//        let defaults = UserDefaults.standard
-//        
-//        if let data = defaults.object(forKey: uniqueIDString) as? Data {
-//            if let uniqueID = NSKeyedUnarchiver.unarchiveObject(with: data) as? String {
-//                self.uniqueID = uniqueID
-//                print("\(#file) > \(#function) > true")
-//                return true
-//            }
-//            print("\(#file) > \(#function) > false, could not unarchive data")
-//            return false
-//        }
-//        print("\(#file) > \(#function) > false, defaults object could not be found")
-//        return false
-//    }
-    
     
     func saveIdentification(peer : MCPeerID, uniqueID: String) {
         print("\(#file) > \(#function) > peerID: \(peer.displayName) and uniqueID: \(uniqueID) are being saved")
